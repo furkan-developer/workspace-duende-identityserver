e42d3f89c2c1c44126b9f7b646f73b633a485a87
furkan aydin <furknn2112@gmail.com>
2024-08-07 20:24:05 +0300

    chore(api1): Configure Jwt Bearer
    - Add required services to DI for authentication and authorization
    - Set Jwt Bearer options according to Duende IdentityServer ApiScopes only.
    - Add required middlewares to middleware pipeline

--- src/API1/Program.cs
+++ src/API1/Program.cs
@@ -1,5 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt => {
+
+        // base-address of your identityserver
+        opt.Authority = "https://localhost:5001";
+        opt.TokenValidationParameters.ValidateAudience = false;
+
+
+        // it's recommended to check the type header to avoid "JWT confusion" attacks
+        opt.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
