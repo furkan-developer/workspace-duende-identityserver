c3c60d1dc4fc1a131e57fb683d2a95378378370b
furkan aydin <furknn2112@gmail.com>
2024-08-07 19:00:41 +0300

    feat(identityserver): Add needed for Client Credential flow
    - Create Duende.IdentityServer.Models.ApiScope to tell the IdentityServer a scope of access
    - Create duende.IdentityServer.Models.Client to tell the IdentityServer a Client

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -12,10 +12,24 @@ namespace IdentityServer
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
-                { };
+                {  new ApiScope(name: "api1", displayName: "My API") };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
-                { };
+                {
+                    new Client()
+                    {
+                        // ha no interactive user, use the clientId and clientSecret for authentication.
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        
+                        ClientId = "client",
+                        ClientSecrets = { new Secret("secret".Sha256()) },
+
+                        // scopes that will use by client for accessing to relevant API that will check access token whether it has api1 scope 
+                        // the list of scopes that the client is allowed to ask for.
+                        // Notice that the allowed scope must match the name of ApiScope above.
+                        AllowedScopes = { "api1"},
+                    }
+                };
     }
 }
