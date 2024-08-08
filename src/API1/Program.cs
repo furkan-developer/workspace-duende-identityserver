b24d2120a03a8664af5413fafa97ef78ac40ae6e
furkan aydin <furknn2112@gmail.com>
2024-08-08 18:59:45 +0300

    docs(api1): Add comment to elaborate JWT Bearer Option
    - Add comment to TokenValidationParameters.ValidateAudience in Program.cs

--- src/API1/Program.cs
+++ src/API1/Program.cs
@@ -8,6 +8,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
         // base-address of your identityserver
         opt.Authority = "https://localhost:5001";
+
+        // Audience validation is disabled here because access to the api is modelled with ApiScopes only (scope-only model).
+        // https://docs.duendesoftware.com/identityserver/v7/quickstarts/1_client_credentials/#add-jwt-bearer-authentication
         opt.TokenValidationParameters.ValidateAudience = false;
 
 
