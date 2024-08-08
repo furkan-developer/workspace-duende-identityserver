231b1a6461bb68993d6a6d49a4a1abf1679e4d13
furkan aydin <furknn2112@gmail.com>
2024-08-08 19:20:37 +0300

    feat(api1): Add '/ api1-scope-policy-restriction' endpoint
    - Create authorization policy that checks whether incoming access token
    has api1 value of scope claim and own of access token is authenticated
    - Create an endpoint which is GET http request
    - Add Authorization policy created endpoint specifing name of the policy

--- src/API1/Program.cs
+++ src/API1/Program.cs
@@ -18,7 +18,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         opt.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
     });
 
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(opt => {
+    opt.AddPolicy("ApiScope", policy => {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "api1");
+    });
+});
 
 var app = builder.Build();
 
@@ -29,5 +34,8 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/identity", (ClaimsPrincipal user) => user.Claims.Select(c => new {c.Type, c.Value}))
     .RequireAuthorization();
+
+app.MapGet("/api1-scope-policy-restriction", ()=> Results.Ok("The current user's access token involves the api1 value of scope claim"))
+    .RequireAuthorization("ApiScope");
 
 app.Run();
