23b56e0f244eb50c11bfc3ddb984b3a679cc5a92
furkan aydin <furknn2112@gmail.com>
2024-08-08 20:26:22 +0300

    feat(client1): Implement OAuth 2.0 client credentials flow
    - Add functionality to retrieve an access token using the OAuth2 client credentials grant.

--- src/Client1/Program.cs
+++ src/Client1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IdentityModel.Client;
+
+async Task<string?> GetAccessTokenWithClientCredentialsGrantAsync(string issuerHost)
+{
+    var client = new HttpClient();
+    var discoverEndpointResponse = await client.GetDiscoveryDocumentAsync(issuerHost);
+
+    if (discoverEndpointResponse.IsError)
+        throw new Exception(discoverEndpointResponse.Error);
+
+    var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+    {
+        Address = discoverEndpointResponse.TokenEndpoint,
+        ClientId = "client",
+        ClientSecret = "secret",
+        Scope = "api1"
+    });
+
+    if (tokenResponse.IsError)
+        throw new Exception(tokenResponse.Error);
+
+    System.Console.WriteLine($"Incoming Access Token: {tokenResponse.AccessToken}");
+
+    return tokenResponse.AccessToken;
+}
+
+string? accessToken = await GetAccessTokenWithClientCredentialsGrantAsync("https://localhost:5001");
+
+if(accessToken is null)
+    throw new Exception("Incoming Access Token is null");
